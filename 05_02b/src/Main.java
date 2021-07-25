d0a6bf29eed3f0e58a65f73e2dda802b813c57f3
oreo91 <73924170+oreo91@users.noreply.github.com>
2021-07-25 17:22:24 -0400

    Add files via upload

---
+++ 05_02b/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+public static void announceDeveloperTeaTime() {
+    System.out.println("Waiting for Developer Tea time");
+    System.out.println("Type in A random number and press enter to start developer tea time");
+    Scanner input = new Scanner(System.in);
+    input.next();
+    System.out.println("It's developer tea time");
+
+}
+    public static void main(String[] args) {
+
+    }
+
+}
