304837d9132c268236d43904f66adb74a0d05c86
oreo91 <73924170+oreo91@users.noreply.github.com>
2021-07-26 23:08:26 -0400

    Add files via upload

---
+++ 05_02b/05_02b/onMyOwn/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String [] args ) {
+        char ch = 'z';
+        double num = -42937737.9d;
+        float XLDecimalNum = 19.98f;
+        byte exampleNum1;
+        long exampleNum2;
+        short exampleNum3;
+        int exampleNum4;
+
+        exampleNum1 = 127;
+        exampleNum2 = 9223372;
+        exampleNum3 = 32767;
+        exampleNum4 = 21474999;
+
+        System.out.println(exampleNum1+" "+ "is the max number byte can hold");
+        System.out.println(exampleNum2+" "+ "is the max number long can hold");
+        System.out.println(exampleNum3+" "+ "is the max number short can hold");
+        System.out.println(exampleNum4+" "+ "is the max number int can hold");
+        System.out.println("ch holds the value of a character Ex:"+" "+ch);
+
+
+
+
+        }
+
+
+
+    }
+
